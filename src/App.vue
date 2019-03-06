b3d2b1ac505810f11605516638a50e7b125f009c
e <997905119@qq.com>
2019-03-06 16:25:40 +0800

    个人简历

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body{
+  background: ghostwhite
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #582c0f;
+  font-weight: bolder;
+}
+.baseLayout{
+  width: 1000px;
+  margin: auto;
+  background: #fff;
+  padding: 10px 20px;
+  box-shadow: 0px 0px 50px #dcdcdc  
+}
+</style>
